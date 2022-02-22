e49a5bd01299f5c9cb297f3cee1a4137f7cb71b1
aeonic <ericknwash@gmail.com>
2022-02-22 13:09:07 -0600

    Just say no to warnings!

---
+++ src/main/java/design/aeonic/catbottles/recipe/virtual/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package design.aeonic.catbottles.recipe.virtual;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
